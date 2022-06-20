ee91021dd4f00ffe926f7f9afd2b0c1b90944179
Darragh Staunton <d.staunton@codat.io>
2022-06-21 00:03:16 +0100

    Custom exception

---
+++ ElgatoWaveAPI/Models/ElgatoException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace ElgatoWaveSDK.Models
+{
+    [Serializable]
+    public class ElgatoException : Exception
+    {
+        public ElgatoException(string message): base(message)
+        {
+
+        }
+
+        public ElgatoException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        protected ElgatoException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public new void GetObjectData(SerializationInfo info, StreamingContext context) => base.GetObjectData(info, context);
+    }
+}
