5dd032a646882f9a5276c4f8de878ee7318a7ded
Melvin <darraghstaunton100@gmail.com>
2022-07-23 22:05:04 +0100

    Uhhh here's the rest of stable shit

--- ElgatoWaveSDK.Emulator/MainWindow.xaml.cs
+++ ElgatoWaveSDK.Emulator/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace ElgatoWaveSDK.Emulator
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : WinUIEx.WindowEx
     {
         public MainWindow()
         {
